05e57992cdef6dc59aca17a4e95109b8914ac024
Ubuntu <ubuntu@ip-172-31-35-122.eu-west-1.compute.internal>
2025-08-16 07:48:07 +0000

    added shellscript files

---
+++ day02/check_if_motilal_loyal.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+<< readme
+
+This is just for infotainment purpose
+readme
+
+
+read -p "motilal ne kitne laddu khaye: " kitne
+read -p "motilal kitna motha hai"kitna_motha
+
+if [[ $kitne -le 5 ]];
+then 
+	echo "motilal is loyal"
+elif [[ $kitna_motha -lt 70 ]];
+the
+	echo" motilal is true legend"
+else
+	echo "motilal is not loyal"
+
+fi
---
+++ day02/motilal_ki_duniya.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This is motilal ki duniya
+<< comment
+Anything
+written
+here will not be execute
+comment
+
+name="motilal"
+
+echo "Name is $name, and date is $(date)"
+
+#echo "enter the name:"
+
+#read username
+
+read -p "please enter your name:" name
+echo "$name, You will be my first friend with name $name. "
+#echo "You entered $username"
+
+
+echo "The characters in $0 $1"
